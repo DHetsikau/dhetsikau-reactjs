dd6b90726d25ad702132e30c25c97666cba7554a
Dzianis Hetsikau <dhetsikau@gomel.iba.by>
2020-02-06 03:16:08 +0300

    ReactJS - task4 - Add ViewOnly mode for Layout

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,22 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './Card.css';
 import { FaCheck, FaPencilAlt, FaTimes } from 'react-icons/fa';
 
-function Card() {
+const Card = (props) => {
+
     const [cardState, setCardState] = useState({
         isStylized: false,
         isEditable: false,
-        data: [{
-          header: "The Card",
-          title: "Listen up!",
-          body: "Your AD could be placed here.",
-        }],
-        temp: [{
-          header: "",
-          title: "",
-          body: "",
-        }],
+        temp: {},
     });
+
+    const viewOnly = useRef(false);
 
     const switchCardStyleHandler = () => {
       setCardState({
@@ -30,59 +24,50 @@ function Card() {
         ...cardState,
         isStylized: false,
         isEditable: true,
-        temp: cardState.data,
+        temp: {...props.data},
       });
     };
 
     const saveCardBtnHandler = () => {
       setCardState({
         ...cardState,
-        isStylized: false,
         isEditable: false,
-        data: [{
-            header: cardState.temp[0].header,
-            title: cardState.temp[0].title,
-            body: cardState.temp[0].body,
-          }],
       });
+      props.onsave(props.data.id, {...cardState.temp})
     };
 
+    
     const declineCardBtnHandler = () => {
       setCardState({
         ...cardState,
-        isStylized: false,
         isEditable: false,
-        temp: [{
-          header: "",
-          title: "",
-          body: "",
-        }],
+        temp: {},
       });
     };
 
     const cardChangeHandler = (propName, event) => {
       setCardState({
         ...cardState,
-        temp: [{
-          ...cardState.temp[0],
+        temp: {
+          ...cardState.temp,
           [propName]: event.target.value,
-        }],
+        },
       });
     };
 
     const renderEditModeButtons = () => {
-      return (
+      return (!props.disabled) ? (
         <span>
           <FaCheck className="save ic" onClick={saveCardBtnHandler.bind(this)}/>
           <FaTimes className="del ic" onClick={declineCardBtnHandler.bind(this)}/>
         </span>
-      );
+      ) : '';
     };
 
     const renderViewModeButtons = () => {
       return (
         <span>
-          <FaPencilAlt className="edit ic" onClick={editCardBtnHandler.bind(this)}/>
+          {!props.disabled ? <FaPencilAlt className="edit ic" onClick={editCardBtnHandler.bind(this)}/> : ''}
           <input type="checkbox" onChange={switchCardStyleHandler} checked={cardState.isStylized} />
         </span>
       );
@@ -92,17 +77,35 @@ function Card() {
       return srcStr.length > validChars ? srcStr.slice(0, validChars) + '...' : srcStr;
     };
 
+    const setViewOnly = () => {
+      setCardState({
+        ...cardState,
+        isEditable: false,
+        temp: {},
+      });
+      viewOnly.current = false;
+    }
+
+    viewOnly.current = props.disabled;
+
+    useEffect(() => {
+      if (viewOnly.current) {
+        setViewOnly();
+        viewOnly.current =  false;
+      }
+    }, [props.disabled]); // eslint-disable-line
+
     return (
       <div className={"item container card bg-light mb-4 " + (cardState.isStylized ? "border-info" : "border-secondary")}>
         <div className={"card-header text-white " + (cardState.isStylized ? "bg-info" : "bg-dark")}>
           <div className= "ch-align">
-            {cardState.isEditable ?
+            {(cardState.isEditable && !props.disabled) ?
               <input
                 type="text"
                 className="iw-150 ih-20"
                 onChange={cardChangeHandler.bind(this, 'header')}
-                defaultValue={cardState.temp[0].header} /> :
-              maskLabel(cardState.data[0].header, 12)}
+                defaultValue={cardState.temp.header} /> :
+              maskLabel(props.data.header, 12)}
           </div>
           <div className="box">
             {cardState.isEditable ? renderEditModeButtons() : renderViewModeButtons()}
@@ -110,21 +113,21 @@ function Card() {
         </div>
         <div className="card-body">
           <h5 className="card-title">
-            {cardState.isEditable ?
+            {(cardState.isEditable && !props.disabled)?
               <input
                 type="text"
                 className="iw-200 ih-30"
                 onChange={cardChangeHandler.bind(this, 'title')}
-                defaultValue={cardState.temp[0].title} /> :
-              maskLabel(cardState.data[0].title, 12)}
+                defaultValue={cardState.temp.title} /> :
+              maskLabel(props.data.title, 12)}
           </h5>
           <p className="card-text">
-            {cardState.isEditable ?
+            {(cardState.isEditable && !props.disabled) ?
               <textarea
                 className="ta"
                 onChange={cardChangeHandler.bind(this, 'body')}
-                defaultValue={cardState.temp[0].body} /> :
-              maskLabel(cardState.data[0].body, 40)}
+                defaultValue={cardState.temp.body} /> :
+              maskLabel(props.data.body, 40)}
           </p>
         </div>
       </div>
