0ba8bb47df23926f05b8176ce20ac59fd7287857
Dzianis Hetsikau <dhetsikau@gomel.iba.by>
2020-06-13 01:33:38 +0300

    ReactJS - task9 - Adding batch using Context API

--- src/components/Layout/Cockpit/Cockpit.js
+++ src/components/Layout/Cockpit/Cockpit.js
@@ -3,9 +3,12 @@ import React from 'react';
 import ViewOnlyPanel from './ViewOnlyPanel/ViewOnlyPanel';
 import SelectModePanel from './SelectModePanel/SelectModePanel';
 
+import AppContext from '../../../context/app-context';
+
 import './Cockpit.css';
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+  const context = React.useContext(AppContext);
   return (
     <div>
       <ViewOnlyPanel
@@ -16,10 +19,10 @@ const cockpit = (props) => {
         viewMode={props.viewMode}
         selectMode={props.selectMode}
         switchSelectMode={props.switchSelectMode}
-        onDeleteCards={props.onDeleteCards}
-        onAddCards={props.onAddCards}
+        onDeleteCards={context.deleteHandler}
+        onAddCards={context.addHandler}
       />
     </div>
   )};
 
-export default cockpit;
+export default Cockpit;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import {v4 as uuidv4} from 'uuid';
 import './Layout.css';
 
 import ReactHeader from './ReactHeader/ReactHeader';
@@ -7,75 +6,11 @@ import Cockpit  from './Cockpit/Cockpit';
 import CardList from './CardList/CardList';
 
 function Layout() {
+
   const [layoutState, setLayoutState] = useState({
     viewMode: false,
     selectMode: false,
-    cards: [
-      {data: {
-        id: 'qqq',
-        header: "The Card 1",
-        title: "Listen up! 1",
-        body: "Your AD could be placed here. 1", 
-        },
-      isSelected: false,},
-      {data: {
-        id: "www",
-        header: "The Card 2",
-        title: "Listen up! 2",
-        body: "Your AD could be placed here. 2",
-        },
-      isSelected: false,},
-      {data: {
-        id: "eee",
-        header: "The Card 3",
-        title: "Listen up! 3",
-        body: "Your AD could be placed here. 3",
-        },
-      isSelected: false,},
-      {data: {
-        id: "rrr",
-        header: "The Card 4",
-        title: "Listen up! 4",
-        body: "Your AD could be placed here. 4",
-        },
-      isSelected: false,},
-      {data: {
-        id: "ttt",
-        header: "The Card 5",
-        title: "Listen up! 5",
-        body: "Your AD could be placed here. 5",
-        },
-      isSelected: false,},
-      {data: {
-        id: "yyy",
-        header: "The Card 6",
-        title: "Listen up! 6",
-        body: "Your AD could be placed here. 6",
-        },
-      isSelected: false,},
-      {data: {
-        id: "uuu",
-        header: "The Card 7",
-        title: "Listen up! 7",
-        body: "Your AD could be placed here. 7",
-        },
-      isSelected: false,},
-    ],
-});
-
-  const saveChangesHandler = (id, vals) => {
-    const cardIndex = layoutState.cards.findIndex(c => {
-      return c.data.id === id
-    });
-    const card = {...layoutState.cards[cardIndex]};
-    card.data = {...vals};
-    const cards = [...layoutState.cards];
-    cards[cardIndex] = card;
-    setLayoutState({
-      ...layoutState,
-      cards: cards,
-    });
-  }
+  });
 
   const switchViewModeHandler = () => {
     setLayoutState({
@@ -90,56 +25,19 @@ function Layout() {
       selectMode: !layoutState.selectMode,
     });
   }
- 
-  const selectCardHandler = (id, value) =>  {
-    
-    const cards = [...layoutState.cards];
-    const cardIndex = cards.findIndex(c => {
-      return c.data.id === id
-    });
-    const card = cards[cardIndex];
-    card.isSelected = value;
-    cards[cardIndex] = card;
-    setLayoutState({
-      ...layoutState,
-      cards: cards,
-    });
-  }
-
-  const deleteCardsHandler = () => {
-    const cards = [...layoutState.cards].filter(c => {
-      return !c.isSelected
-    });
-    setLayoutState({
-      ...layoutState,
-      cards: cards,
-    });
-  }
-
-  const addCardHandler = () => {
-    setLayoutState({
-      ...layoutState,
-      cards: [...layoutState.cards, {data: { id: uuidv4(),}, isSelected: false,}],
-    });
-  }
 
   return (
     <div>
       <ReactHeader />
       <Cockpit
         viewMode={layoutState.viewMode}
-        switchViewMode={switchViewModeHandler}
         selectMode={layoutState.selectMode}
+        switchViewMode={switchViewModeHandler}
         switchSelectMode={switchSelectModeHandler}
-        onDeleteCards = {deleteCardsHandler}
-        onAddCards = {addCardHandler}
       />
       <CardList 
-        cards={layoutState.cards}
-        onSaveCard={saveChangesHandler}
         viewMode={layoutState.viewMode}
         selectMode={layoutState.selectMode}
-        onCheckCard={selectCardHandler}
       />
     </div>
   )
